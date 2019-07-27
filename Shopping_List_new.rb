582d3c1aef97f8cf27f64ccdd827fc0cec540846
Abhinav Kannojia <abhinav.kannojia1@gmail.com>
2019-07-27 00:44:17 -0700

    Shopping list updated - simplified

---
+++ Shopping_List_new.rb
@@ -0,0 +1,31 @@
+class ShoppingList
+ attr_reader :list
+ attr_accessor :total
+
+ def initialize
+   @list = []
+   @total = 0
+ end
+
+ def add(items, quantity)
+   if @list.include?(items) == false
+     @list << [items, quantity]
+   else
+     @list
+   end
+ end
+
+ def total
+  quantities = list.map { |item| item[1] }
+  total = quantities.inject(0){|sum,x| sum + x }
+ end
+end
+
+sl = ShoppingList.new
+
+sl.add('Toothpaste', 2)
+sl.add('Computer', 1)
+sl.add('Toothpaste', 3)
+
+puts sl.list
+puts sl.total
