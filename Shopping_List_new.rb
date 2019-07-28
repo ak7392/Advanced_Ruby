04c02923f2f7c2fbd44a482079e5cb2802d1db69
Abhinav Kannojia <abhinav.kannojia1@gmail.com>
2019-07-27 17:15:42 -0700

    Shopping List with repititive call

--- Shopping_List_new.rb
+++ Shopping_List_new.rb
@@ -15,6 +15,10 @@ class ShoppingList
    end
  end
 
+ def items(&block)
+   block.call
+ end
+
  def total
   quantities = list.map { |item| item[1] }
   total = quantities.inject(0){|sum,x| sum + x }
@@ -23,9 +27,11 @@ end
 
 sl = ShoppingList.new
 
-sl.add('Toothpaste', 2)
-sl.add('Computer', 1)
-sl.add('Toothpaste', 3)
+sl.items do
+  sl.add('Toothpaste', 2)
+  sl.add('Computer', 1)
+  sl.add('Toothpaste', 3)
+end
 
 puts sl.list
 puts sl.total
