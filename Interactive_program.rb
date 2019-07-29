54a7c630080b7d188d656bd50485d831bf96521b
Abhinav Kannojia <abhinav.kannojia1@gmail.com>
2019-07-28 22:31:06 -0700

    Interactive Program

--- Interactive_program.rb
+++ Interactive_program.rb
@@ -1,14 +1,10 @@
-class Bind
-  def get_binding
-    binding
-  end
-  def exit_method
-    exit(0)
-  end
+require 'irb'
+class C
+    def my_method
+        @var = 'hi'
+        $my_binding = binding
+        IRB.start(__FILE__)
+    end
 end
 
-if ARGV[0] == "q"
-  Bind.new.exit_method
-else
-  puts eval("ARGV[0]", Bind.new.get_binding)
-end
+C.new.my_method
