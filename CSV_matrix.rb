373f64d2711ad96da4ceb4910794a90afe3bd0a7
Abhinav Kannojia <abhinav.kannojia1@gmail.com>
2019-08-10 19:02:08 -0700

    CSV_matrix

--- CSV_matrix.rb
+++ CSV_matrix.rb
@@ -1,52 +1,23 @@
 require 'csv'
 
-class FeedGenerator
-  attr_accessor :source, :feed, :current_row
-
-  def initialize(source_name, feed_name)
-    @source = CSV.read(source_name, headers: true)
-    @current_row = Row.new
-    @feed = []
-
-    # define method by header of source file
-    @source.headers.each do |column|
-      @current_row.instance_eval do
-        define_singleton_method(column) do
-          self.row[column]
-        end
-      end
-    end
-
-    @feed = CSV.open(feed_name, 'wb+')
-    @feed << columns
-  end
-
-  def columns
-     %w(name age city)
-  end
-
-  def generate_feed
-    @source.each do |row|
-      @current_row.row = row
-      @feed << columns.map do |c|
-        @current_row.send(c)
-      end
-    end
-  end
-
-  def columns; raise 'unimplimented'; end
-
-  class Row
-    attr_accessor :row
-
-    # you can define unrecognized column mapping below
-    def name; name; end
-    def age; age; end
-    def city; city; end
+class ReadCsv
+  def self.read(file_name)
+    class_name = File.basename(file_name, ".csv").capitalize
+    rows = CSV.read(file_name)
+    the_class = Struct.new(*rows.shift.map(&:to_sym))
+    Object.const_set(class_name, the_class)
+    rows.map { |row| the_class.new(*row) }
   end
 end
-class StarkFeedGenerator < FeedGenerator
-  def columns; %w(name); end
-end
 
-StarkFeedGenerator.new('Input.csv','new_feed.csv').generate_feed
+array = ReadCsv.read('Input.csv')
+first_row = array[0]
+p first_row
+p first_row.name
+p first_row.name = 'XYZ'
+p first_row.age = 25
+second_row = array[1]
+p second_row
+p second_row.name
+p second_row.name = 'Abhinav_Kannojia'
+p second_row.age = 26
